027c183e083a9ed9741e88839fdce2a3659ef09b
Boris Erbesfeld <Boris@Cyberbit.com>
2018-04-10 20:02:35 +0300

    Updated project for Visual Studio 2015 & WDK 10

--- TestDriver/Common.h
+++ TestDriver/Common.h
@@ -20,25 +20,3 @@
 #endif
 
 #define POOL_TAG ('thgT')
-
-#define DEVICE_NAME			L"\\Device\\TestKLibght"
-#define DEVICE_DOS_NAME		L"\\DosDevices\\TestKLibght"
-
-typedef struct _DRIVER_GLOBALS
-{
-	PDEVICE_OBJECT	ptDeviceObject;
-	BOOLEAN			bSymbolicLinkCreated;
-} DRIVER_GLOBALS, *PDRIVER_GLOBALS;
-
-EXTERN_C DRIVER_GLOBALS g_tDriverGlobals;
-
-#define IO_COMPLETE_IRP(ptIrp, eStatus, dwInformation)			\
-	{															\
-		if (NULL != ptIrp)										\
-		{														\
-			(ptIrp)->IoStatus.Information = (dwInformation);	\
-			(ptIrp)->IoStatus.Status = (eStatus);				\
-			IoCompleteRequest((ptIrp), IO_NO_INCREMENT);		\
-		}														\
-	}
-
